5f8bc53296d66747003376149afcb118d3bb0216
Vedangi Wagh <40696836+vedangiwagh@users.noreply.github.com>
2023-11-29 23:27:48 -0500

    setup

---
+++ wisebites/src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
