91339d48b5a269e5e83ba57f5e30f2d0e0cfdd21
Vedangi Wagh <40696836+vedangiwagh@users.noreply.github.com>
2023-12-05 22:23:07 -0500

    added filter in recipe

--- wisebites/src/pages/Recipes.js
+++ wisebites/src/pages/Recipes.js
@@ -1,63 +1,114 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Recipes.css'; // Import your CSS file for styling
 import { BrowserRouter as Router, Routes, Route , Link} from 'react-router-dom';
+
+
 const Recipes = () => {
-  const [recipes, setRecipes] = useState([]);
+   const [recipes, setRecipes] = useState([]);
+   const [filteredRecipes, setFilteredRecipes] = useState([]);
+    const [client, setClient] = useState('');
 
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/recipes/getRecipesForClient', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            mealType: 'Main Course',
-            recipeType: 'Vegetarian',
-            client: 2,
-          }),
-        });
+    const [name, setName] = useState('');
 
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+    useEffect(() => {
+     setClient(2);
+      const fetchRecipes = async () => {
+        try {
+          const response = await fetch('http://localhost:8080/recipes/getRecipesForClient', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              mealType: 'Main Course',
+              recipeType: 'Vegetarian',
+              client: 2,
+            }),
+          });
+
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+
+          const data = await response.json();
+          setRecipes(data);
+//          setFilteredRecipes(data);
+        } catch (error) {
+          console.error('Error fetching recipes:', error);
         }
+      };
 
-        const data = await response.json();
-        setRecipes(data);
-      } catch (error) {
-        console.error('Error fetching recipes:', error);
-      }
-    };
+      fetchRecipes();
+    }, []); // Empty dependency array means the effect runs once after the first render
 
-    fetchRecipes();
-  }, []); // Empty dependency array means the effect runs once after the first render
+const handleSearch = async () => {
+    try {
+if (name === null || name === '') {
+      // If name is null or an empty string, set an empty array and return
+      setFilteredRecipes([]);
+      return;
+    }
+      const response = await fetch(`http://localhost:8080/recipes/byNameAndClient/${name}/${client}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      console.log('Fetched Data:', data);
+      setFilteredRecipes(data || []);
+    } catch (error) {
+      console.error('Error fetching filtered recipes:', error);
+      setFilteredRecipes([]);
+    }
+  };
+    return (
+      <div className="recipes-page">
+        <h1>Recipes</h1>
+         <label>
+                Search for recipe:
+                 <input type="text" value={name} placeholder="Type the recipe name" onChange={handleNameChange} />
+              </label>
 
-  return (
-    <div className="recipes-page">
-      <h1>Recipes</h1>
-      <div className="recipe-container">
-        {recipes.map((recipe) => (
-          <RecipeCard key={recipe.recipeId} {...recipe} />
-        ))}
+               <button onClick={handleSearch}>Submit</button>
+        <div className="recipe-container">
+         {filteredRecipes.length === 0 ? (
+         <ul>
+                {recipes.map((recipe) => (
+                            <RecipeCard key={recipe.recipeId} {...recipe} />
+                          ))}
+                          </ul>
+              ) : (
+<ul>
+                  {filteredRecipes.map((recipe) => (
+                             <RecipeCard key={recipe.recipeId} {...recipe} />
+                         ))}
+              </ul>
+              )}
+
+        </div>
+
+        <Link to="/">
+                <button className="home-button">Go to Home</button>
+              </Link>
       </div>
-      <Link to="/">
-              <button className="home-button">Go to Home</button>
-            </Link>
-    </div>
-  );
-};
+    );
+  };
 
-const RecipeCard = ({ recipeName, description, instructions, preparationTime, cookingTime }) => {
-  return (
-    <div className="recipe-card">
-      <h2>{recipeName}</h2>
-      <p>{description}</p>
-      <p>Instructions: {instructions}</p>
-      <p>Preparation Time: {preparationTime} minutes</p>
-      <p>Cooking Time: {cookingTime} minutes</p>
-    </div>
-  );
-};
+  const RecipeCard = ({ recipeName, description, instructions, preparationTime, cookingTime }) => {
+    return (
+      <div className="recipe-card">
+        <h2>{recipeName}</h2>
+        <p>{description}</p>
+        <p>Instructions: {instructions}</p>
+        <p>Preparation Time: {preparationTime} minutes</p>
+        <p>Cooking Time: {cookingTime} minutes</p>
+      </div>
+    );
+  };
 
 export default Recipes;
